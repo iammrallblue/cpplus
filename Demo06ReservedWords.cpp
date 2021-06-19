590c537e32741d09df48e2757ef8bbd8079dda56
joh <iammrallblue@gmail.com>
2021-06-19 09:23:28 -0400

    c++

---
+++ Demo06ReservedWords.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    Reserved Words in C++:
+	asm, inline, typeid, dynamic_cast, typedef, typename, signed, 
+	sizeof, union, mutable, unsigned, using, namespace, explicit,
+	static_cast, export, struct, extern, virtual, const_cast, void,
+	volatile, wchar_t, register, reinterpret_cast, goto, delete, 
+	operator, template, this,
+
+    Key points:
+        1. DO NOT use keyword as variable name
+*/
+
+int main()
+{
+    int int_number = 10;
+    // int int = 10 // error
+    return 0;
+}
