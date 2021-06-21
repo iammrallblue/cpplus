a10b4f5028b58a44d19dcbff2f8b5676a30590fd
joh <iammrallblue@gmail.com>
2021-06-20 23:04:30 -0400

    Day01

---
+++ Day01/Demo14Input.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+    Data input:
+        1. Integer
+            int x = 0;
+
+        2. Float point
+        3. Character
+        4. String
+        5. Boolean
+*/
+int main()
+{
+    // 1. Integer
+    // int x = 0;
+    // cout << "Input a number: " << endl;
+    // cin >> x;
+    // cout << "The input number is: " << x << endl;
+
+    // // 2. Float point
+    // float y = 3.24F;
+    // cout << "Input a float number: " << endl;
+    // cin >> y;
+    // cout << "The input float number is: " << y << endl;
+
+    // 3. Character
+    // char ch = 'a';
+    // cout << "Input a letter: " << endl;
+    // cin >> ch;
+    // cout << "The input letter is: " << ch << endl;
+    
+    // 4. String
+    string str = "Bella";
+    cout << "Input a string: " << endl;
+    cin >> str;
+    cout << "The input string is: " << str << endl;
+
+    return 0;
+}
