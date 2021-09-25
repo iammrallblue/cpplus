0566cbd916b6ee6aa01848df3ee6a1821a4d04db
joh <iammrallblue@gmail.com>
2021-09-25 10:22:11 -0400

    Class07

--- src/Class07/labq1.cpp
+++ src/Class07/labq1.cpp
@@ -15,6 +15,7 @@ using namespace std;
 
     2 1 55 3 2 1 4 4 2 2 55
 */
+
 int main()
 {
     cout << "Input a string of integers seprated by space: " << endl;
@@ -27,16 +28,41 @@ int main()
     // cout << "str_line: " << str_line << endl;
 
     int number = 0;
+    char temp;
 
+    istringstream ss(str_line);
     // iterate elements from the string str_line by eachfor
-    for (auto &x : str_line)
+    while (ss >> temp)
     {
-        // check all elements by xor operator
-        number ^= ((int)x - 48);
-    } // for
-
+        number ^= (temp - '0');
+    }
     cout << "The exclusive number is: " << number << endl;
-    ;
 
-    return EXIT_SUCCESS;
 } // main
+
+// version 2
+// int main()
+// {
+//     cout << "Input a string of integers seprated by space: " << endl;
+
+//     // a string for storing input
+//     string str_line;
+
+//     // call the getline function to save the input to variable str_line
+//     getline(cin, str_line);
+//     // cout << "str_line: " << str_line << endl;
+
+//     int number = 0;
+
+//     // iterate elements from the string str_line by eachfor
+//     for (auto &x : str_line)
+//     {
+//         // check all elements by xor operator
+//         number ^= ((int)x - 48);
+//     } // for
+
+//     cout << "The exclusive number is: " << number << endl;
+//     ;
+
+//     return EXIT_SUCCESS;
+// } // main
