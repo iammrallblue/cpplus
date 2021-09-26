62f012c99b1c41a92064bfbcd9684d3f0424cdb3
joh <iammrallblue@gmail.com>
2021-09-26 11:57:45 -0400

    Day05

--- src/Day05/Demo01Pointer.cpp
+++ src/Day05/Demo01Pointer.cpp
@@ -8,6 +8,30 @@ using namespace std;
         A pointer variable is a variable whose contents are of pointer type
 
         How to define and use pointer:
+        
+        Reference Operator (&):
+            Ampersand sign (&), read as "address of"
+                int myvar = 25;
+                bar = myvar; // 25 to bar 
+
+                foo = &myvar; // the address of myvar to foo
+                "&myvar" read as: assign the address of the variable "myvar" to foo
+                variable "foo" is what in C++ is called a POINTER
+        
+        Dereference Operator (*):
+            A variable which stores the address of another variable is called a POINTER.
+            Pointers are said to "point to" the variable whose address they store.
+            
+            The operator itself can be read as "value pointed to by".
+                baz = *foo;
+                This read as: "set baz equal to the value pointed to by foo",
+
+            Ex: ***
+                variable myvar = 25; assgin value 25 to variable myvar
+                &myvar = 1776; "1776" which is the address of variable
+                foo = &myvar; assign the address of variable myvar to foo
+                *foo = 25;  
+
 
         Syntax:
             int * pointerName;
@@ -34,6 +58,10 @@ using namespace std;
 
             4. Indrection operator " * ".
                 commonly referred to as the indirection operator or dereferencing operator
+
+            5. int *p1, *p2 is not same as int *p1, p2;
+                    int *p1, and *p2 are two pointers,
+                    int *p1, and p2 are one point *p1 and another one is variable p2
 
 */
 int main()
