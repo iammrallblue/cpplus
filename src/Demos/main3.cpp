8d59622958a0d89476c5375ea6c1f991afe59a83
joh <iammrallblue@gmail.com>
2021-10-04 12:40:14 -0400

    Demos

---
+++ src/Demos/main3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char ch = ' ';
+    cout << "blank space is: " << int(ch) << endl; // 32
+
+    int i = 2;
+    cout << "the ASCII of 2: " << char(2) << endl;
+} // main
