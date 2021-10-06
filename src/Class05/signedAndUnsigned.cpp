629f68bb1ad92d4c62ab012a86fd11753bb69be5
joh <iammrallblue@gmail.com>
2021-10-06 08:50:36 -0400

    Class05

--- src/Class05/signedAndUnsigned.cpp
+++ src/Class05/signedAndUnsigned.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <cstdlib>
+#include <climits>
 
 using namespace std;
 
 /*
     Signed and unsigned in C++:
+        to print out max and min values of signed and unsigned variables
 
+            method 1:
+                int x;
+                    x - 1;
 
 */
 
@@ -18,6 +23,14 @@ int main(int argc, char const *argv[])
 
     i = j;
 
+    short unsigned x = (short)9999999;
+    cout << "The max value of short is: " << x << endl;
+
     cout << "i is: " << i << " j is: " << j << endl;
+
+    cout << "The max value of unsigned int: " << numeric_limits<unsigned int>::max() << endl;
+    cout << "The min value : " << numeric_limits<unsigned int>::min() << endl;
+
+    // cout<<std::numeric_limits<int>::min()<<endl;
     return 0;
 }
