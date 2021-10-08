94580d7af92a62c7c6a17175bec93d149d946196
joh <iammrallblue@gmail.com>
2021-10-07 21:51:10 -0400

    Class03

--- src/Class03/CharType.cpp
+++ src/Class03/CharType.cpp
@@ -4,19 +4,30 @@ using namespace std;
 
 /*
     Ex. 
-        converting char type to acsii value;
+        converting char type to ascii value;
+
+        Arithmetic in char type
 */
 int main(int argc, char const *argv[])
 {
     char ch;
-    int acsii_value;
+    int ascii_value;
 
     cout << "input a character: " << endl;
     cin >> ch;
 
-    // converting the input character to acsii value
-    acsii_value = (int)ch;
+    // converting the input character to ascii value
+    ascii_value = (int)ch;
 
-    cout << "The ACSII value of the character " << ch << " is: " << acsii_value << endl;
+    cout << "The ASCII value of the character " << ch << " is: " << ascii_value << endl;
+
+    // Arithmetic in char type
+    char ch1, ch2;
+    ch1 = 'a';
+    ch2 = 'a' + 1; // output letter 'b'
+
+    cout << ch2 << endl;     // 'a' + 1 = 'b'
+    cout << ch1 + 1 << endl; // output number 98
+
     return 0;
 }
