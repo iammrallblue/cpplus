667fca3b4d6e0b4f43026dc9c6caa1773dd3a0eb
joh <iammrallblue@gmail.com>
2021-10-13 16:02:41 -0400

    Day01

--- src/Day01/Demo08sizeof.cpp
+++ src/Day01/Demo08sizeof.cpp
@@ -16,16 +16,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Size of char : " << sizeof(char) << endl;
-    cout << "Size of int : " << sizeof(int) << endl;
-    cout << "Size of short int : " << sizeof(short int) << endl;
-    cout << "Size of long int : " << sizeof(long int) << endl;
-    cout << "Size of float : " << sizeof(float) << endl;
-    cout << "Size of double : " << sizeof(double) << endl;
-    cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+    cout << "Size of char : " << sizeof(char) << endl;           // 1 byte
+    cout << "Size of int : " << sizeof(int) << endl;             // 4 bytes
+    cout << "Size of short int : " << sizeof(short int) << endl; // 2 bytes
+    cout << "Size of long int : " << sizeof(long int) << endl;   // 8 bytes
+    cout << "Size of float : " << sizeof(float) << endl;         // 4 bytes
+    cout << "Size of double : " << sizeof(double) << endl;       // 8 bytes
+    cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;     // 4 bytes
 
     int int_var = 100;
-    cout << "Size of int_var: " << sizeof(int_var) << endl;
-    
+    cout << "Size of int_var: " << sizeof(int_var) << endl; // 4 bytes, an int variable
+
     return 0;
 }
