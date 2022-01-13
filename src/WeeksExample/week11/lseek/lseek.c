5e7956b2653996379ccdadb62ab80a9f5db7be48
joh <iammrallblue@gmail.com>
2022-01-13 08:27:55 -0500

    lseek.c

--- src/WeeksExample/week11/lseek/lseek.c
+++ src/WeeksExample/week11/lseek/lseek.c
@@ -4,25 +4,13 @@
 
 int main()
 {
-	char *filename = "example.txt";
+	char* filename = "example.txt";
 	int fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		perror("open");
-	}
+	if (fd == -1) perror("open");
 
 	off_t size = lseek(fd, 0, SEEK_END);
-
-	if (size == -1)
-	{
-		perror("lseek");
-	}
-	else
-	{
-		printf("The size of %s is %ld bytes\n", filename, size);
-	}
+	if (size == -1) perror("lseek");
+	else printf("The size of %s is %ld bytes\n", filename, size);
 
 	close(fd); // could do error checking here
-
-	return 0;
 } // main
